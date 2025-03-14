dd4b878ec7b196f97cbd925f0f029dd76cf19b70
= <=>
2025-03-14 15:01:11 +0900

    임시커밋

---
+++ BluePen-master/BluePen/src/test/java/com/cothink/bluepen/BluePenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cothink.bluepen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluePenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
