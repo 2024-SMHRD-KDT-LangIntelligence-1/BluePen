b3a564dc0c5546938f46a5e1b0669a7bb57f7ce8
= <=>
2025-03-14 16:48:19 +0900

    데이터베이스 연결

--- BluePen-master/BluePen/src/main/java/com/cothink/bluepen/BluePenApplication.java
+++ BluePen-master/BluePen/src/main/java/com/cothink/bluepen/BluePenApplication.java
@@ -2,9 +2,8 @@ package com.cothink.bluepen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class BluePenApplication {
 
 	public static void main(String[] args) {
