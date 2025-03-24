fdbdfa1a229ad753f8d08b12263c4e72246d1b46
smhrd <smhrd@DESKTOP-FCQFD2K.mshome.net>
2025-03-24 16:55:37 +0900

    aaa

--- BluePen/src/main/java/LicenseCodeAPI/TechQualificationAPI.java
+++ BluePen/src/main/java/LicenseCodeAPI/TechQualificationAPI.java
@@ -8,5 +8,28 @@ import java.net.URL;
 import java.net.URLEncoder;
 
 public class TechQualificationAPI {
-
+	public static void main(String[] args) throws IOException {
+        StringBuilder urlBuilder = new StringBuilder("http://openapi.q-net.or.kr/api/service/rest/InquiryTestInformationNTQSVC/getJMList"); /*URL*/
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=xpiTjg/jQSwlHvprRxo8ARUahlPYJPptVfcvMMg8GDl9zp+n3vcDKrRVpSH9lflZVOk2eegHCc4MwAw8URQ01A=="); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("jmcd","UTF-8") + "=" + URLEncoder.encode("1320", "UTF-8")); /*종목코드*/
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        System.out.println("Response code: " + conn.getResponseCode());
+        BufferedReader rd;
+        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+        rd.close();
+        conn.disconnect();
+        System.out.println(sb.toString());
+    }
 }
