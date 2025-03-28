f2757ee64ba804adc804ed0057151fca5ab6f2a5
SMHRD <SMHRD@DESKTOP-71H2CRA>
2025-03-28 17:37:30 +0900

    리스트-캘린더 연결, 리스트 페이지 수정

--- BluePen/src/main/java/com/cothink/bluepen/repository/ScheduleRepo.java
+++ BluePen/src/main/java/com/cothink/bluepen/repository/ScheduleRepo.java
@@ -2,9 +2,14 @@ package com.cothink.bluepen.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.cothink.bluepen.entity.Tblschedule;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cothink.bluepen.entity.Tblschedule;
 
 @Repository
 public interface ScheduleRepo extends JpaRepository<Tblschedule, Integer> {
 }
+
